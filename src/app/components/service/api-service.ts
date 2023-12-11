1e87e7f3d981f80c3cb438582497d87b978859ac
Nikita <thekillermaps@gmail.com>
2023-12-11 21:28:14 +0200

    api_url to host

--- src/app/components/service/api-service.ts
+++ src/app/components/service/api-service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class ApiService {
-  public static API_URL = 'http://localhost:8080/api';
+	public static API_URL = 'https://alexandria-backend.onrender.com/api';
 }
